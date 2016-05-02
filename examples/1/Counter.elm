acbe476d9d93ebc8c0aec09b790fb83eb7c97634
Glen Mailer <glenjamin@gmail.com>
2016-05-02 20:45:27 +0100

    Put my own stamp on the counter

--- examples/1/Counter.elm
+++ examples/1/Counter.elm
@@ -1,4 +1,4 @@
-module Counter where
+module Counter (..) where
 
 import Html exposing (..)
 import Html.Attributes exposing (style)
@@ -7,12 +7,19 @@ import Html.Events exposing (onClick)
 
 -- MODEL
 
-type alias Model = Int
+
+type alias Model =
+  Int
+
 
 
 -- UPDATE
 
-type Action = Increment | Decrement
+
+type Action
+  = Increment
+  | Decrement
+
 
 update : Action -> Model -> Model
 update action model =
@@ -24,23 +31,36 @@ update action model =
       model - 1
 
 
+
 -- VIEW
+
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  div []
+  div
+    [ divStyle ]
     [ button [ onClick address Decrement ] [ text "-" ]
     , div [ countStyle ] [ text (toString model) ]
     , button [ onClick address Increment ] [ text "+" ]
     ]
 
 
+divStyle : Html.Attribute
+divStyle =
+  style
+    [ ( "margin", "20px" )
+    , ( "padding", "20px" )
+    , ( "border", "1px solid black" )
+    , ( "float", "left" )
+    ]
+
+
 countStyle : Attribute
 countStyle =
   style
-    [ ("font-size", "20px")
-    , ("font-family", "monospace")
-    , ("display", "inline-block")
-    , ("width", "50px")
-    , ("text-align", "center")
+    [ ( "font-size", "20px" )
+    , ( "font-family", "monospace" )
+    , ( "display", "inline-block" )
+    , ( "width", "50px" )
+    , ( "text-align", "center" )
     ]
