c1ccd4e1ef4e61c4e331cb2e72339371f8c215a7
Glen Mailer <glenjamin@gmail.com>
2016-05-02 20:45:37 +0100

    What if counter 3 used a dictionary?

--- examples/3/Counter.elm
+++ examples/3/Counter.elm
@@ -1,4 +1,4 @@
-module Counter (Model, init, Action, update, view) where
+module Counter (..) where
 
 import Html exposing (..)
 import Html.Attributes exposing (style)
@@ -7,16 +7,23 @@ import Html.Events exposing (onClick)
 
 -- MODEL
 
-type alias Model = Int
+
+type alias Model =
+  Int
 
 
 init : Int -> Model
-init count = count
+init =
+  identity
+
 
 
 -- UPDATE
 
-type Action = Increment | Decrement
+
+type Action
+  = Increment
+  | Decrement
 
 
 update : Action -> Model -> Model
@@ -29,23 +36,37 @@ update action model =
       model - 1
 
 
+
 -- VIEW
+
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  div []
+  div
+    [ divStyle ]
     [ button [ onClick address Decrement ] [ text "-" ]
     , div [ countStyle ] [ text (toString model) ]
     , button [ onClick address Increment ] [ text "+" ]
     ]
 
 
+divStyle : Html.Attribute
+divStyle =
+  style
+    [ ( "margin", "20px" )
+    , ( "padding", "20px" )
+    , ( "border", "1px solid black" )
+    , ( "float", "left" )
+    , ( "clear", "left" )
+    ]
+
+
 countStyle : Attribute
 countStyle =
   style
-    [ ("font-size", "20px")
-    , ("font-family", "monospace")
-    , ("display", "inline-block")
-    , ("width", "50px")
-    , ("text-align", "center")
+    [ ( "font-size", "20px" )
+    , ( "font-family", "monospace" )
+    , ( "display", "inline-block" )
+    , ( "width", "50px" )
+    , ( "text-align", "center" )
     ]
--- examples/3/CounterList.elm
+++ examples/3/CounterList.elm
@@ -1,72 +1,97 @@
-module CounterList where
+module CounterList (..) where
 
+import Dict exposing (Dict)
 import Counter
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes exposing (style)
 import Html.Events exposing (..)
 
 
 -- MODEL
 
-type alias Model =
-    { counters : List ( ID, Counter.Model )
-    , nextID : ID
-    }
 
-type alias ID = Int
+type alias Model =
+  { counters : Dict ID Counter.Model
+  , nextID : ID
+  }
+
+
+type alias ID =
+  Int
 
 
 init : Model
 init =
-    { counters = []
-    , nextID = 0
-    }
+  { counters = Dict.empty
+  , nextID = 0
+  }
+
 
 
 -- UPDATE
 
+
 type Action
-    = Insert
-    | Remove
-    | Modify ID Counter.Action
+  = Insert
+  | Remove
+  | Modify ID Counter.Action
 
 
 update : Action -> Model -> Model
 update action model =
   case action of
     Insert ->
-      let newCounter = ( model.nextID, Counter.init 0 )
-          newCounters = model.counters ++ [ newCounter ]
+      let
+        newCounters =
+          Dict.insert model.nextID (Counter.init 0) model.counters
       in
-          { model |
-              counters = newCounters,
-              nextID = model.nextID + 1
-          }
+        { model
+          | counters = newCounters
+          , nextID = model.nextID + 1
+        }
 
     Remove ->
-      { model | counters = List.drop 1 model.counters }
+      let
+        key =
+          Dict.keys model.counters |> List.foldl min 99999999
+      in
+        { model | counters = Dict.remove key model.counters }
 
     Modify id counterAction ->
-      let updateCounter (counterID, counterModel) =
-              if counterID == id then
-                  (counterID, Counter.update counterAction counterModel)
-              else
-                  (counterID, counterModel)
+      let
+        modification =
+          Maybe.map <| Counter.update counterAction
       in
-          { model | counters = List.map updateCounter model.counters }
+        { model | counters = Dict.update id modification model.counters }
+
 
 
 -- VIEW
 
+
 view : Signal.Address Action -> Model -> Html
 view address model =
-  let counters = List.map (viewCounter address) model.counters
-      remove = button [ onClick address Remove ] [ text "Remove" ]
-      insert = button [ onClick address Insert ] [ text "Add" ]
+  let
+    counters =
+      Dict.map (viewCounter address) model.counters
+        |> Dict.toList
+        |> List.sortBy fst
+        |> List.map snd
+        |> List.reverse
+
+    remove =
+      button [ onClick address Remove ] [ text "Remove" ]
+
+    insert =
+      button [ onClick address Insert ] [ text "Add" ]
   in
-      div [] ([remove, insert] ++ counters)
+    div [] ([ remove, insert ] ++ counters)
 
 
-viewCounter : Signal.Address Action -> (ID, Counter.Model) -> Html
-viewCounter address (id, model) =
-  Counter.view (Signal.forwardTo address (Modify id)) model
+viewCounter : Signal.Address Action -> ID -> Counter.Model -> Html
+viewCounter address id model =
+  div
+    [ style [ ( "overflow", "hidden" ) ] ]
+    [ (text <| toString id)
+    , Counter.view (Signal.forwardTo address (Modify id)) model
+    ]
